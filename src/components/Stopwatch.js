9d1218634448a4a0fa7727d63a6501883107c734
William Olojede <wolojede@yahoo.com>
2017-03-15 05:26:18 +0300

    made the stopwatch tick

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -5,24 +5,47 @@ class Stopwatch extends Component{
 	    super(props);
 	    this.state = {
 	      running: false,
+	      elapsedTime: 0,
+	      previousTime: 0,
 	    };
-	  }
+	}
+	componentDidMount = () => {
+		this.interval = setInterval(this.onTick, 100);
+	}
+	componentWillUnmount = () => {
+		clearInterval(this.interval);
+	}
+	onTick = () => {
+		if(this.state.running) {
+			var now = Date.now();
+			this.setState({
+				previousTime: now,
+				elapsedTime: this.state.elapsedTime + (now -this.state.previousTime), 
+			})
+		}
+	}
+	onStop = () => {
+		this.setState({ running: false });
+	}
+	onStart = () => {
+		this.setState({ 
+			running: true,
+			previousTime: Date.now(), 
+		});
+	}
+	onReset = () => {
+		this.setState({
+			elapsedTime: 0,
+			previousTime: Date.now(),
+		})
+	}
 
-	  onStop = () => {
-			this.setState({ running: false });
-		}
-		onStart = () => {
-			this.setState({ running: true });
-		}
-		onReset = () => {
-
-		}
-	  
 	render () {
+		var seconds = Math.floor(this.state.elapsedTime / 1000);
 		return(
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
-				<div className="stopwatch-time">0</div>
+				<div className="stopwatch-time">{seconds}</div>
 				{ this.state.running ?
 				  <button onClick={this.onStop}>Stop</button>
 				  :
